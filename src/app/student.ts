0d18773e2d916999ebe9b7f359262dea96986b23
Shubham Srivastava <shubhamsrivastava@administrators-MacBook-Air.local>
2022-07-13 17:33:54 +0530

    Initial Commit

---
+++ src/app/student.ts
@@ -0,0 +1,9 @@
+export interface Student {
+    id: number;
+    name: string;
+    email: string;
+    characteristic: string;
+    phone: string;
+    imageUrl: string;
+    rollNumber: string;
+}
